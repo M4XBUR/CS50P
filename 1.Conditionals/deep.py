06ab4404b11d56b60be2b2dd7b0620900423bbd3
MAXBUR <maximinchannel@gmail.com>
2025-02-22 17:35:17 +0300

    deep

---
+++ 1.Conditionals/deep.py
@@ -0,0 +1,10 @@
+user_text = input('What is the Answer to the Great Question of Life, the Universe, and Everything? ')
+
+if user_text == '42':
+	print('Yes')
+elif user_text == 'Forty Two':
+	print('Yes')
+elif user_text == 'forty-two':
+	print('Yes')
+else:
+	print('No')
