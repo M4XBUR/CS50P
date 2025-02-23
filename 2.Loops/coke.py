067b822c02dec79c6d8c0f4744dd9f571dada769
MAXBUR <maximinchannel@gmail.com>
2025-02-23 19:43:32 +0300

    coke

---
+++ 2.Loops/coke.py
@@ -0,0 +1,9 @@
+amount_due = 50
+
+while amount_due > 0:
+	print(f'Amount Due: {amount_due}')
+	coin = int(input('Insert coin: '))
+	if coin in [5, 10, 25]:
+		amount_due -= coin
+
+print(f'Change Owed:', abs(amount_due))
