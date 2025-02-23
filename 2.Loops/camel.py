5f76036b511dd0f25c3f1c94ab61d1383a9df1f0
MAXBUR <maximinchannel@gmail.com>
2025-02-23 18:39:49 +0300

    camel

--- 2.Loops/camel.py
+++ 2.Loops/camel.py
@@ -1,13 +1,13 @@
 def main():
 	camelCase = input('camelCase: ')
-	print(f'shake_case: ', end='')
+	print('shake_case: ', end='')
 	convert(camelCase)
 
 
 def convert(text):
 	for i in text:
 		if i.isupper():
-			print(f'_{i.lower()}', end='')
+			print(i.lower(), end='')
 		else:
 			print(i, end='')
 
