256fc89d2d98eee0ce686f2eb8338cfea3e321e8
MAXBUR <maximinchannel@gmail.com>
2025-03-11 21:30:05 +0300

    correction professor

--- 4.Libraries/professor.py
+++ 4.Libraries/professor.py
@@ -1,46 +1,38 @@
 from random import randint
 
 def main():
-    generate_integer(get_level())
+    level = get_level()
+    score = 0
+    for _ in range(10):
+        x = generate_integer(level)
+        y = generate_integer(level)
+        correct_answer = x + y
+        try:
+            user_answer = int(input(f'{x} + {y} = '))
+        except ValueError:
+            print('EEE')
+            continue
+        if user_answer == correct_answer:
+            score += 1
+        else:
+            print('EEE')
+    print(f'Score: {score}')
 
 
 def get_level():
     while True:
-        input_level = input('Level: ')
-        if input_level in ['1', '2', '3']:
-            return input_level
-        else:
-            continue
+        level = input('Level: ')
+        if level in ['1', '2', '3']:
+            return level
 
 
 def generate_integer(level):
-    count_game = 0
-    while count_game <= 10:
-        if level == '1':
-            x = randint(0,9)
-            y = randint(0,9)
-        elif level == '2':
-            x = randint(10,99)
-            y = randint(10,99)
-        elif level == '3':
-            x = randint(100,999)
-            y = randint(100,999)
-        
-        score = 0
-        try:
-            print(f'{x} + {y} = ', end='')
-            user_answer = int(input(''))
-        except ValueError:
-            print('EEE')
-            continue
-        if user_answer == x + y:
-            score += 1
-            count_game += 1
-        else:
-            print('EEE')
-            count_game += 1
-            continue
-    print(f'Score: {score}')
+    if level == '1':
+        return randint(0,9)
+    elif level == '2':
+        return randint(10,99)
+    elif level == '3':
+        return randint(100,999)  
 
 
 if __name__ == "__main__":
